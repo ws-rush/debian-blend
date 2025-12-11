8fad30c9aee12cd23cbbf2613e1d942de0b4fdb0
ws-rush <wusaby.rush@zohomail.com>
2025-12-11 20:31:06 +0300

    feat: Add Readex Pro font package and import its styles.

--- page/src/index.tsx
+++ page/src/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/readex-pro/200.css';
+import '@fontsource/readex-pro/300.css';
+import '@fontsource/readex-pro/400.css';
+import '@fontsource/readex-pro/500.css';
+import '@fontsource/readex-pro/600.css';
+import '@fontsource/readex-pro/700.css';
 import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
